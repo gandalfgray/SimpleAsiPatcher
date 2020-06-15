e16349ebdaf76e804537aca30fa45dda57b1389b
Verok <verokster@gmail.com>
2020-06-15 04:40:15 +0300

    added Castle's Lighthouse building bonus

---
+++ src/H3Structures.h
@@ -0,0 +1,118 @@
+struct H3Army
+{
+ INT32 type[7];
+ INT32 count[7];
+};
+
+struct H3Artifact
+{
+ INT32 id;
+ INT32 mod;
+};
+
+struct H3Town
+{
+ UINT8 id;
+ INT8 owner_id;
+ INT8 built_this_turn;
+ INT8 field_03;
+ INT8 type;
+ INT8 x;
+ INT8 y;
+ INT8 z;
+ INT8 boat_x;
+ INT8 boat_y;
+ INT16 field_0A;
+ INT32 up_hero_id;
+ INT32 down_hero_id;
+ INT8 mag_level;
+ INT8 field_15;
+ INT16 available_creatures[14];
+ INT8 fields_32[6];
+ DWORD field_38;
+ DWORD field_3C;
+ INT16 field_40;
+ INT16 field_42;
+ DWORD spells[5][6];
+ INT8 magic_hild[5];
+ INT8 fields_C1[7];
+ CHAR name[12];
+ INT32 _u8[3];
+ H3Army guards; //+E0 = охрана замка
+ H3Army guards0; //+118 = охрана замка
+ DWORD built_bits; //*B +150h = уже построенные здания (0400)
+ DWORD built_bits2;
+ DWORD bonus_bits;//*B +158h = бонус на существ, ресурсы и т.п., вызванный строениями
+ DWORD bonus_bits2;
+ DWORD available_bits;      //*B- +160h = маска доступных для строения строений
+ DWORD available_bits2;     
+};
+
+struct H3Hero
+{
+ INT16 x; // +0
+ INT16 y; // +2
+ INT16 z; // +4
+ UINT8 visible; // +6
+ DWORD mui_xyz; // +7
+ INT8 field_0B; // +11
+ DWORD miu_object_type; // +12
+ DWORD miu_object_c_flag; // +16
+ DWORD miu_setup; // +20
+ UINT16 spell_points; // +24
+ DWORD id; // +26
+ DWORD id_wtf; // +30
+ INT8 owner_id; // +34
+ CHAR name[13]; // +35
+ DWORD _class; // +48
+ INT8 pic; // +52
+ DWORD aim_x; // +53
+ DWORD aim_y; // +57
+ DWORD aim_z; // +61
+ INT8 field_41[3]; // +65
+ INT8 x_0; // +68
+ INT8 y_0; // +69
+ INT8 run; // +70
+ INT8 field_47; // +71
+ INT8 flags; // +72
+ DWORD movement_points_max; // +73
+ DWORD movement_points; // +77
+ DWORD expa; // +81
+ INT16 level; // +85
+ DWORD visited[10]; // +87
+ INT8 field_7F[18]; // +127
+ H3Army army; // +145
+ INT8 second_skill[28]; // +201
+ INT8 second_skill_show[28]; // +229
+ DWORD second_skill_count; // +257
+ DWORD temp_mod_flags; // + 261	
+ float ai_exp_eff_109;
+ INT8 dd_cast_this_turn; // +269
+ DWORD disguise; // +270
+ DWORD fly; // +274
+ DWORD waterwalk; // +278
+ INT8 d_morale_1; // +282
+ INT8 d_luck; // +283
+ INT8 is_sleeping_byte11C; // +284
+ INT8 field_11E[16]; // +284
+ H3Artifact doll_art[19]; // +301
+ INT8 free_add_slots; // +453
+ INT8 locked_slot[14]; // +454
+ H3Artifact backpack_art[64]; // +468
+ INT8 backpack_arts_count; // +980
+ DWORD sex; // +981
+ bool has_biography; // +985
+ INT8 biography[16]; // +986
+ INT8 spell[70]; // +1002
+ INT8 spell_level[70]; // +1072
+ INT8 attack; // +1142
+ INT8 defence; // +1143
+ INT8 power; // +1144
+ INT8 knowledge; // +1145
+ DWORD ai_agress_47A; // Agressivness + 1146
+ DWORD ai_power_eff_47E; // PowerEffectivness +1150
+ DWORD ai_duration_eff_482; // DurationEffectivness +1154
+ DWORD ai_knowl_eff_486; // KnowledgeEffectivness +1158
+ DWORD ai_dmana_eff_48A; // DoubleManaEffectivness +1162
+ DWORD ai_mana_eff_48E; // ManaEffectivness +1164
+};
--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,7 @@
 
 #include "windows.h"
 #include "hooker.h"
+#include "H3Structures.h"
 
 #pragma comment(lib, "kernel32.lib")
 
@@ -37,6 +38,10 @@ const struct AddressSpace {
 	DWORD pattern_check[2];
 	DWORD armorer_fix[3];
 	DWORD creatures_luck[2];
+	struct {
+		DWORD hook;
+		DWORD type;
+	} lighthouse_bonus;
 } addressSpace[] = {
 // === RUS ======================================================================================================================================
 #pragma region RUS
@@ -44,26 +49,31 @@ const struct AddressSpace {
 	0x004D4A5F, 0x0059308B,
 	0x00000000, 0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
+	0x00000000, 0,
 
 	// Heroes III Armageddon - v2.1 Buka
 	0x004F2533, 0x005F9649,
 	0x0041D5B3, 0x0041D6F1, 0x004640FD,
 	0x00440545, 0x0043E686,
+	0x004DFE30, 0,
 
 	// Heroes III Armageddon - v2.2 Buka
 	0x004F2863, 0x005F9609,
 	0x0041D553, 0x0041D691, 0x00463D9D,
 	0x00440045, 0x0043E186,
+	0x004E02F0, 0,
 
 	// Heroes III Shadow - v3.1 Buka
 	0x004F7EB3, 0x00602379,
 	0x0041E2D3, 0x0041E40F, 0x00465EF3,
 	0x00441DA4, 0x0043FEC2,
+	0x004E4A20, 0,
 
 	// Heroes III Complete - v4.0 Buka
 	0x004F7EB3, 0x006021A9,
 	0x0041E456, 0x0041E595, 0x0046592C,
 	0x00441626, 0x0043F7B2,
+	0x004E4ED0, 0,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -71,11 +81,13 @@ const struct AddressSpace {
 	0x004EBA34, 0x005AF329,
 	0x0041D5F3, 0x0041D731, 0x00461206,
 	0x0043E0F6, 0x0043FFB5,
+	0x004D9107, 0,
 
 	// Heroes Chronicles Beastmaster & Sword - v1.0
 	0x004EB494, 0x005AF2D9,
 	0x0041D6A3, 0x0041D7E1, 0x00461126,
 	0x0043FD05, 0x0043DE46,
+	0x004D8EC7, 0,
 #pragma endregion
 
 // === ENG ======================================================================================================================================
@@ -84,26 +96,31 @@ const struct AddressSpace {
 	0x004D3363, 0x0058E558,
 	0x00000000, 0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
+	0x00000000, 0,
 
 	// Heroes III Erathia - v1.1
 	0x0041E573, 0x004210B8,
 	0x00000000, 0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
+	0x00000000, 0,
 
 	// Heroes III Erathia - v1.2
 	0x0041E523, 0x00421078,
 	0x00000000, 0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
+	0x00000000, 0,
 
 	// Heroes III Erathia - v1.3
 	0x004F58F3, 0x005D9679,
 	0x0041E003, 0x0041E13F, 0x00465203,
 	0x00000000, 0x00000000,
+	0x004E27A0, 0,
 
 	// Heroes III Erathia - v1.4
 	0x004F5583, 0x005D8F69,
 	0x0041E343, 0x0041E47F, 0x004653D3,
 	0x00000000, 0x00000000,
+	0x004E25F0, 0,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -111,16 +128,19 @@ const struct AddressSpace {
 	0x004EB283, 0x005EEFD8,
 	0x0041DDE3, 0x0041DF1F, 0x00460C59,
 	0x00440850, 0x0043E992,
+	0x004D91D0, 1,
 
 	// Heroes III Armageddon - v2.1
 	0x004F5C43, 0x00600299,
 	0x0041E343, 0x0041E47F, 0x00465A23,
 	0x004416D4, 0x0043F7F2,
+	0x004E2D90, 0,
 
 	// Heroes III Armageddon - v2.2
 	0x004F5963, 0x005FFBF9,
 	0x0041E033, 0x0041E16F, 0x00465A53,
 	0x00441904, 0x0043FA22,
+	0x004E2940, 0,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -128,16 +148,19 @@ const struct AddressSpace {
 	0x004F7D73, 0x006027E9,
 	0x0041E4B3, 0x0041E5EF, 0x00465D63,
 	0x00441A64, 0x0043FB82,
+	0x004E4930, 0,
 
 	// Heroes III Shadow - v3.1
 	0x004F85B3, 0x006027E9,
 	0x0041E203, 0x0041E33F, 0x00465B23,
 	0x004414A4, 0x0043F5C2,
+	0x004E5130, 0,
 
 	// Heroes III Shadow - v3.2
 	0x004F8193, 0x00602149,
 	0x0041E3A3, 0x0041E4DF, 0x00465943,
 	0x00441524, 0x0043F642,
+	0x004E4D40, 0,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -145,33 +168,39 @@ const struct AddressSpace {
 	0x004F7B03, 0x00601B89,
 	0x0041E223, 0x0041E35F, 0x00465D63,
 	0x00441804, 0x0043F922,
+	0x004E4AD0, 0,
 
 	// ==============================================================================================================================================
 
 	// Heroes Chronicles Warlords & Underworld - v1.0
 	0x004F0033, 0x005B5129,
 	0x0041E1A3, 0x0041E2DF, 0x0046364C,
-	0x441D34, 0x43FE52,
+	0x00441D34, 0x0043FE52,
+	0x004DCB70, 1,
 
 	// Heroes Chronicles Elements & Dragons - v1.0
 	0x004EFE04, 0x005B5469,
 	0x0041E1C3, 0x0041E2FF, 0x004632CC,
 	0x00441744, 0x0043F862,
+	0x004DCB30, 1,
 
 	// Heroes Chronicles WorldTree - v1.0
 	0x004EFA84, 0x005B51B9,
 	0x0041E163, 0x0041E29F, 0x0046326C,
 	0x004418F4, 0x0043FA12,
+	0x004DC5E0, 1,
 
 	// Heroes Chronicles FieryMoon - v1.0
 	0x004EF824, 0x005B5249,
 	0x0041E183, 0x0041E2BF, 0x00462CDC,
 	0x00441424, 0x0043F542,
+	0x004DC760, 1,
 
 	// Heroes Chronicles Beastmaster & Sword - v1.0
 	0x004EF874, 0x005B4C09,
 	0x0041E213, 0x0041E34F, 0x00462E5C,
 	0x00441514, 0x0043F632,
+	0x004DC670, 1,
 #pragma endregion
 
 // === USA ======================================================================================================================================
@@ -180,6 +209,7 @@ const struct AddressSpace {
 	0x004EF914, 0x005B51B9,
 	0x0041E433, 0x0041E56F, 0x0046345C,
 	0x004419E4, 0x0043FB02,
+	0x004DC850, 1,
 #pragma endregion
 
 // === GER ======================================================================================================================================
@@ -188,6 +218,7 @@ const struct AddressSpace {
 	0x004D5253, 0x00591B29,
 	0x00000000, 0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
+	0x00000000, 0,
 
 	// ==============================================================================================================================================
 
@@ -195,6 +226,7 @@ const struct AddressSpace {
 	0x004EFA04, 0x005B51C9,
 	0x0041E0F3, 0x0041E22F, 0x00462C0C,
 	0x00441354, 0x0043F472,
+	0x004DC940, 1,
 #pragma endregion
 
 // === FRA ======================================================================================================================================
@@ -203,11 +235,13 @@ const struct AddressSpace {
 	0x004F61C3, 0x006003D9,
 	0x0041E143, 0x0041E27F, 0x00465CB3,
 	0x00441914, 0x0043FA32,
+	0x004E3050, 0,
 
 	// Heroes III Shadow - v3.1
 	0x004F8163, 0x006028F9,
 	0x0041E413, 0x0041E54F, 0x004661B3,
 	0x00441CF4, 0x0043FE12,
+	0x004E4A20, 0,
 #pragma endregion
 
 // === POL ======================================================================================================================================
@@ -216,25 +250,69 @@ const struct AddressSpace {
 	0x004F5723, 0x005FED57,
 	0x0041E383, 0x0041E4BF, 0x00465813,
 	0x004414A4, 0x0043F5C2,
+	0x004E2890, 0,
 
 	// Heroes III Shadow - v3.1
 	0x004F7AF3, 0x00600ED7,
 	0x0041E233, 0x0041E36F, 0x00465AE3,
 	0x00441574, 0x0043F692,
+	0x004E44D0, 0,
 
 	// Heroes III Shadow - v3.2
 	0x004F78D3, 0x00602179,
 	0x0041E353, 0x0041E48F, 0x00465973,
 	0x004414E4, 0x0043F602,
+	0x004E4480, 0,
 
 	// Heroes III Shadow - v3.2 / Armageddon - v2.2
 	0x004F5993, 0x005FE337,
 	0x0041E1A3, 0x0041E2DF, 0x00465903,
-	0x00441764, 0x0043F883
+	0x00441764, 0x0043F883,
+	0x004E2B20, 0
 #pragma endregion
 };
 
-VOID PatchSpace(HOOKER hooker, const AddressSpace* hookSpace)
+const AddressSpace* hookSpace;
+
+#pragma region The Castles Lighthouse building bonus
+DWORD __fastcall castleOwnerCheck(H3Town* town, H3Hero* hero)
+{
+	return hookSpace->lighthouse_bonus.hook + (hero->owner_id == town->owner_id ? 6 : 44);
+}
+
+VOID __declspec(naked) hook_castleOwnerCheck_v0()
+{
+	__asm
+	{
+		push ecx
+		mov edx, [ebp - 0x4]
+		call castleOwnerCheck
+		pop ecx
+
+		mov edx, eax
+		mov eax, [ecx + 0x150]
+		jmp edx
+	}
+}
+
+VOID __declspec(naked) hook_castleOwnerCheck_v1()
+{
+	__asm
+	{
+		push edx
+		mov ecx, [ebp - 0x4]
+		xchg ecx, edx
+		call castleOwnerCheck
+		pop edx
+
+		mov ecx, eax
+		mov eax, [edx + 0x150]
+		jmp ecx
+	}
+}
+#pragma endregion
+
+VOID PatchSpace(HOOKER hooker)
 {
 	// Armorer fix
 	if (hookSpace->armorer_fix[0])
@@ -245,11 +323,15 @@ VOID PatchSpace(HOOKER hooker, const AddressSpace* hookSpace)
 	if (hookSpace->creatures_luck[0])
 		for (DWORD i = 0; i < sizeof(hookSpace->creatures_luck) / sizeof(DWORD); ++i)
 			PatchNop(hooker, hookSpace->creatures_luck[i], 6);
+
+	// Castle's Lighthouse building bonus
+	if (hookSpace->lighthouse_bonus.hook)
+		PatchHook(hooker, hookSpace->lighthouse_bonus.hook, !hookSpace->lighthouse_bonus.type ? hook_castleOwnerCheck_v0 : hook_castleOwnerCheck_v1);
 }
 
 extern "C" VOID __stdcall PatchMain(HOOKER hooker)
 {
-	const AddressSpace* hookSpace = addressSpace;
+	hookSpace = addressSpace;
 	DWORD count = sizeof(addressSpace) / sizeof(AddressSpace);
 	do
 	{
@@ -257,7 +339,7 @@ extern "C" VOID __stdcall PatchMain(HOOKER hooker)
 		if ((ReadDWord(hooker, hookSpace->pattern_check[0] + 1, &check) && check == (WS_VISIBLE | WS_POPUP)) &&
 			(ReadDWord(hooker, hookSpace->pattern_check[1] + 1, &check) && check == (WS_VISIBLE | WS_POPUP)))
 		{
-			PatchSpace(hooker, hookSpace);
+			PatchSpace(hooker);
 			break;
 		}
 
