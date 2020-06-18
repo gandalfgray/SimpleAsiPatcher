6a32d0c7331ae2af28db16eebce8e9cc11e6dd1d
Verok <verokster@gmail.com>
2020-06-19 00:31:40 +0300

    1.04 - added Tavern rumors bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
 	MIT License
 
-	Copyright (c) 2020 Oleksiy Ryabchun
+	Copyright (c) 2020 Verok & Ben80
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
@@ -22,9 +22,17 @@
 	SOFTWARE.
 */
 
-// VS 6.0  > cl main.cpp /nologo /MD /W0 /O1 /GF /link /out:h3_fix.asi /debug:none /dll /merge:.text=.asi /merge:.rdata=.asi /section:.asi,re
-// VS 2008 > cl main.cpp /nologo /MD /GS- /GL /O1 /Os /GF /link /out:h3_fix.asi /debug:none /manifest:no /merge:.text=.asi /merge:.rdata=.asi /section:.asi,re
-// VS 2019 > cl main.cpp /nologo /MD /GS- /GL /O1 /Os /GF /link /out:h3_fix.asi /debug:none /emitpogophaseinfo /merge:.text=.rdata /merge:.rdata=.asi /section:.asi,re
+// VS 6.0
+// > rc main.rc
+// > cl main.cpp main.res /nologo /MD /W0 /O1 /GF /link /out:h3_fix.asi /debug:none /dll /merge:.text=.asi /merge:.rdata=.asi /section:.asi,re
+
+// VS 2008
+// > rc main.rc
+// > cl main.cpp main.res /nologo /MD /GS- /GL /O1 /Os /GF /link /out:h3_fix.asi /debug:none /manifest:no /merge:.text=.asi /merge:.rdata=.asi /section:.asi,re
+
+// VS 2019
+// > rc main.rc
+// > cl main.cpp main.res /nologo /MD /GS- /GL /O1 /Os /GF /link /out:h3_fix.asi /debug:none /emitpogophaseinfo /merge:.text=.rdata /merge:.rdata=.asi /section:.asi,re
 
 #define WIN32_LEAN_AND_MEAN
 
@@ -50,6 +58,7 @@ const struct AddressSpace {
 		DWORD skip;
 		BYTE type;
 	} ai_water_walk_fly;
+	DWORD tavern_rumors;
 } addressSpace[] = {
 // === RUS ======================================================================================================================================
 #pragma region RUS
@@ -59,6 +68,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x00000000, 0,
 	0x00000000, 0x00000000, 0x00000000, 1,
+	0x004B0570,
 
 	// Heroes III Armageddon - v2.1 Buka
 	0x004F2533, 0x005F9649,
@@ -66,6 +76,7 @@ const struct AddressSpace {
 	0x00440545, 0x0043E686,
 	0x004DFE30, 0,
 	0x0042EDC8, 0x0042EDEB, 0x0042F10B, 0,
+	0x004C8550,
 
 	// Heroes III Armageddon - v2.2 Buka
 	0x004F2863, 0x005F9609,
@@ -73,6 +84,7 @@ const struct AddressSpace {
 	0x00440045, 0x0043E186,
 	0x004E02F0, 0,
 	0x0042ED98, 0x0042EDBB, 0x0042F0DB, 0,
+	0x004C87E0,
 
 	// Heroes III Shadow - v3.1 Buka
 	0x004F7EB3, 0x00602379,
@@ -80,6 +92,7 @@ const struct AddressSpace {
 	0x00441DA4, 0x0043FEC2,
 	0x004E4A20, 0,
 	0x004300DE, 0x00430101, 0x00430410, 0,
+	0x004CCD40,
 
 	// Heroes III Complete - v4.0 Buka
 	0x004F7EB3, 0x006021A9,
@@ -87,6 +100,7 @@ const struct AddressSpace {
 	0x00441626, 0x0043F7B2,
 	0x004E4ED0, 0,
 	0x004301E6, 0x00430209, 0x00430519, 0,
+	0x004CCFE0,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -96,6 +110,7 @@ const struct AddressSpace {
 	0x0043E0F6, 0x0043FFB5,
 	0x004D9107, 0,
 	0x0042ED78, 0x0042ED9B, 0x0042F0BB, 0,
+	0x004C1150,
 
 	// Heroes Chronicles Beastmaster & Sword - v1.0
 	0x004EB494, 0x005AF2D9,
@@ -103,6 +118,7 @@ const struct AddressSpace {
 	0x0043FD05, 0x0043DE46,
 	0x004D8EC7, 0,
 	0x0042EE68, 0x0042EE8B, 0x0042F1AB, 0,
+	0x004C1410,
 #pragma endregion
 
 // === ENG ======================================================================================================================================
@@ -113,6 +129,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x00000000, 0,
 	0x0042E079, 0x0042E09C, 0x0042E39B, 0,
+	0x004AF690,
 
 	// Heroes III Erathia - v1.1
 	0x0041E573, 0x004210B8,
@@ -120,6 +137,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x00000000, 0,
 	0x0044DA49, 0x0044DA6C, 0x0044DD6B, 0,
+	0x004C11D0,
 
 	// Heroes III Erathia - v1.2
 	0x0041E523, 0x00421078,
@@ -127,6 +145,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x00000000, 0,
 	0x0044D8E9, 0x0044D90C, 0x0044DC0B, 0,
+	0x004C1410,
 
 	// Heroes III Erathia - v1.3
 	0x004F58F3, 0x005D9679,
@@ -134,6 +153,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x004E27A0, 0,
 	0x0042FE4E, 0x0042FE71, 0x00430180, 0,
+	0x004CAA90,
 
 	// Heroes III Erathia - v1.4
 	0x004F5583, 0x005D8F69,
@@ -141,6 +161,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x004E25F0, 0,
 	0x0043017E, 0x004301A1, 0x004304B0, 0,
+	0x004CA6C0,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -150,6 +171,7 @@ const struct AddressSpace {
 	0x00440850, 0x0043E992,
 	0x004D91D0, 1,
 	0x0042F8F9, 0x0042F91C, 0x0042FC1B, 0,
+	0x004C3290,
 
 	// Heroes III Armageddon - v2.1
 	0x004F5C43, 0x00600299,
@@ -157,6 +179,7 @@ const struct AddressSpace {
 	0x004416D4, 0x0043F7F2,
 	0x004E2D90, 0,
 	0x0043015E, 0x00430181, 0x00430490, 0,
+	0x004CAF80,
 
 	// Heroes III Armageddon - v2.2
 	0x004F5963, 0x005FFBF9,
@@ -164,6 +187,7 @@ const struct AddressSpace {
 	0x00441904, 0x0043FA22,
 	0x004E2940, 0,
 	0x0042FE6E, 0x0042FE91, 0x004301A0, 0,
+	0x004CB020,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -173,6 +197,7 @@ const struct AddressSpace {
 	0x00441A64, 0x0043FB82,
 	0x004E4930, 0,
 	0x004301CE, 0x004301F1, 0x00430500, 0,
+	0x004CCC30,
 
 	// Heroes III Shadow - v3.1
 	0x004F85B3, 0x006027E9,
@@ -180,6 +205,7 @@ const struct AddressSpace {
 	0x004414A4, 0x0043F5C2,
 	0x004E5130, 0,
 	0x0043001E, 0x00430041, 0x00430350, 0,
+	0x00000000,
 
 	// Heroes III Shadow - v3.2
 	0x004F8193, 0x00602149,
@@ -187,6 +213,7 @@ const struct AddressSpace {
 	0x00441524, 0x0043F642,
 	0x004E4D40, 0,
 	0x0043020E, 0x00430231, 0x00430540, 0,
+	0x004CCC40,
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -196,6 +223,7 @@ const struct AddressSpace {
 	0x00441804, 0x0043F922,
 	0x004E4AD0, 0,
 	0x0043020E, 0x00430231, 0x00430540, 0,
+	0x004CCF80,
 
 	// ==============================================================================================================================================
 
@@ -205,6 +233,7 @@ const struct AddressSpace {
 	0x00441D34, 0x0043FE52,
 	0x004DCB70, 1,
 	0x0042FF0E, 0x0042FF31, 0x00430240, 0,
+	0x004C5160,
 
 	// Heroes Chronicles Elements & Dragons - v1.0
 	0x004EFE04, 0x005B5469,
@@ -212,6 +241,7 @@ const struct AddressSpace {
 	0x00441744, 0x0043F862,
 	0x004DCB30, 1,
 	0x0042FFEE, 0x00430011, 0x00430320, 0,
+	0x004C4B90,
 
 	// Heroes Chronicles WorldTree - v1.0
 	0x004EFA84, 0x005B51B9,
@@ -219,6 +249,7 @@ const struct AddressSpace {
 	0x004418F4, 0x0043FA12,
 	0x004DC5E0, 1,
 	0x0042FEBE, 0x0042FEE1, 0x004301F0, 0,
+	0x004C4AF0,
 
 	// Heroes Chronicles FieryMoon - v1.0
 	0x004EF824, 0x005B5249,
@@ -226,6 +257,7 @@ const struct AddressSpace {
 	0x00441424, 0x0043F542,
 	0x004DC760, 1,
 	0x0042FEDE, 0x0042FF01, 0x00430210, 0,
+	0x004C4810,
 
 	// Heroes Chronicles Beastmaster & Sword - v1.0
 	0x004EF874, 0x005B4C09,
@@ -233,6 +265,7 @@ const struct AddressSpace {
 	0x00441514, 0x0043F632,
 	0x004DC670, 1,
 	0x0042FF46, 0x0042FF69, 0x00430279, 0,
+	0x004C4520,
 #pragma endregion
 
 // === USA ======================================================================================================================================
@@ -243,6 +276,7 @@ const struct AddressSpace {
 	0x004419E4, 0x0043FB02,
 	0x004DC850, 1,
 	0x0043018E, 0x004301B1, 0x004304C0, 0,
+	0x004C4B40,
 #pragma endregion
 
 // === GER ======================================================================================================================================
@@ -253,6 +287,7 @@ const struct AddressSpace {
 	0x00000000, 0x00000000,
 	0x00000000, 0,
 	0x0042E5FB, 0x0042E61E, 0x0042E94D, 0,
+	0x004B1500,
 
 	// ==============================================================================================================================================
 
@@ -262,6 +297,7 @@ const struct AddressSpace {
 	0x00441354, 0x0043F472,
 	0x004DC940, 1,
 	0x0042FE4E, 0x0042FE71, 0x00430180, 0,
+	0x004C43C0,
 #pragma endregion
 
 // === FRA ======================================================================================================================================
@@ -272,6 +308,7 @@ const struct AddressSpace {
 	0x00441914, 0x0043FA32,
 	0x004E3050, 0,
 	0x0042FF5E, 0x0042FF81, 0x00430290, 0,
+	0x004CB170,
 
 	// Heroes III Shadow - v3.1
 	0x004F8163, 0x006028F9,
@@ -279,6 +316,7 @@ const struct AddressSpace {
 	0x00441CF4, 0x0043FE12,
 	0x004E4A20, 0,
 	0x0043022E, 0x00430251, 0x00430560, 0,
+	0x004CD040,
 #pragma endregion
 
 // === POL ======================================================================================================================================
@@ -289,6 +327,7 @@ const struct AddressSpace {
 	0x004414A4, 0x0043F5C2,
 	0x004E2890, 0,
 	0x004301BE, 0x004301E1, 0x004304F0, 0,
+	0x004CA910,
 
 	// Heroes III Shadow - v3.1
 	0x004F7AF3, 0x00600ED7,
@@ -296,6 +335,7 @@ const struct AddressSpace {
 	0x00441574, 0x0043F692,
 	0x004E44D0, 0,
 	0x00430026, 0x00430049, 0x00430359, 0,
+	0x004CCA10,
 
 	// Heroes III Shadow - v3.2
 	0x004F78D3, 0x00602179,
@@ -303,13 +343,15 @@ const struct AddressSpace {
 	0x004414E4, 0x0043F602,
 	0x004E4480, 0,
 	0x00430136, 0x00430159, 0x00430469, 0,
+	0x004CC630,
 
 	// Heroes III Shadow - v3.2 / Armageddon - v2.2
 	0x004F5993, 0x005FE337,
 	0x0041E1A3, 0x0041E2DF, 0x00465903,
 	0x00441764, 0x0043F883,
 	0x004E2B20, 0,
-	0x0042FF86, 0x0042FFA9, 0x004302B9, 0
+	0x0042FF86, 0x0042FFA9, 0x004302B9, 0,
+	0x004CAB10
 #pragma endregion
 };
 #pragma pack()
@@ -450,6 +492,14 @@ VOID PatchSpace(HOOKER hooker)
 	// Prevents AI from casting Fly if they don't have it.
 	if (hookSpace->ai_water_walk_fly.hook)
 		PatchHook(hooker, hookSpace->ai_water_walk_fly.hook, !hookSpace->ai_water_walk_fly.type ? hook_ai_water_walk_fly_v0 : hook_ai_water_walk_fly_v1);
+
+	// Tavern rumors bug fix
+	if (hookSpace->tavern_rumors)
+	{
+		DWORD arrList;
+		if (ReadDWord(hooker, hookSpace->tavern_rumors + 3, &arrList))
+			PatchDWord(hooker, hookSpace->tavern_rumors + 3, arrList - 4);
+	}
 }
 
 extern "C" VOID __stdcall PatchMain(HOOKER hooker)
